6621377c49e5fda1f19db46dd03976b8dfbe73e3
SaphirTPW <nath.bonkena@gmail.com>
2025-07-28 16:59:45 +0200

    Player Actor Update -->
    - Add Player Controller Input
    - Add Player Move Method
    - Add Boolean in Player Controller to give the permission to move the player actor

--- Assets/Prototype/1. Code/Controllers/PlayerController.cs
+++ Assets/Prototype/1. Code/Controllers/PlayerController.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Puiblic Variables 
+    #region Public Variables 
+    public Vector2 InputDirection { get => _inputDirection; set => _inputDirection = value; }
     #endregion
 
     #region Private Variables 
+    private Vector2 _inputDirection;
+    [SerializeField] private bool _canMove;
     #endregion
 
     #region Unity Methods 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -18,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMoveInput();
     }
     #endregion
 
@@ -26,6 +29,14 @@ public class PlayerController : MonoBehaviour
     #endregion
 
     #region Private Methods 
+    private void HandleMoveInput()
+    {
+        if (_canMove) 
+        {
+            _inputDirection.x = Input.GetAxis("Horizontal");
+            _inputDirection.y = Input.GetAxis("Vertical");
+        }
+    }
     #endregion
 
     #region Coroutines
--- Assets/Prototype/1. Code/Physics/PlayerMovement.cs
+++ Assets/Prototype/1. Code/Physics/PlayerMovement.cs
@@ -1,18 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    #region Puiblic Variables 
+    #region Public Variables 
     #endregion
 
     #region Private Variables 
+    private PlayerController _pc;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _speedMod;
+    [SerializeField][Range(0, 0.3f)] private float _playerMoveSmoothing;
+    private Vector3 _velocity = Vector3.zero;
     #endregion
 
     #region Unity Methods 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        _pc = GetComponent<PlayerController>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,12 +30,23 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(_pc.InputDirection.x);
+    }
     #endregion
 
     #region Public Methods 
     #endregion
 
     #region Private Methods 
+    private void MovePlayer(float pDirection)
+    {
+        Vector2 targetVelocity = new Vector2(pDirection * _playerSpeed * _speedMod, _rb.linearVelocityY);
+        _rb.linearVelocity = Vector3.SmoothDamp(_rb.linearVelocity, targetVelocity, ref _velocity, _playerMoveSmoothing);
+    }
+    
     #endregion
 
     #region Coroutines
