e4c138b78bd41d1e6f81a91fa9a582a8de68531f
SaphirTPW <nath.bonkena@gmail.com>
2025-07-28 15:35:33 +0200

    Set Up Project Workflow
    - Added dedicated folder for the project
    - Created Player Controller
    - Created Player Movement
    - Set Up The PlayRoom Scene

---
+++ Assets/Prototype/1. Code/Physics/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Puiblic Variables 
+    #endregion
+
+    #region Private Variables 
+    #endregion
+
+    #region Unity Methods 
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public Methods 
+    #endregion
+
+    #region Private Methods 
+    #endregion
+
+    #region Coroutines
+    #endregion
+}
