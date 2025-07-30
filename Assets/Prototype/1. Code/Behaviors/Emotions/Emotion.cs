4c15b77c3874af76c66040230ae09f6f7d7b7ff9
SaphirTPW <nath.bonkena@gmail.com>
2025-07-30 21:56:02 +0200

    Player Actor Updated --> Player Health
    - Added Player Health Script
    - Updated Player Actor prefab
    - Start Set Up Emotion Script

--- Assets/Prototype/1. Code/Behaviors/Emotions/Emotion.cs
+++ Assets/Prototype/1. Code/Behaviors/Emotions/Emotion.cs
@@ -6,17 +6,39 @@ using UnityEngine;
 public class Emotion : MonoBehaviour
 {
     #region Public Variables 
+    public EmotionState EmoState { get => _emotionState; set => _emotionState = value; }
+    public float CurrentEmotionEnergy { get => _currentEmotionEnergy; set => _currentEmotionEnergy = value; }
     #endregion
 
     #region Private Variables 
     [SerializeField] private EmotionData _emotionData;
     private PlayerMovement _pm;
     private PlayerCombat _pmCom;
+    private EmotionController _ec;
+
+    [SerializeField] private EmotionState _emotionState;
+    [SerializeField] private float _maxEmotionEnergy = 100f;
+    [SerializeField] private float _currentEmotionEnergy;
+    [SerializeField] private float _buildUpRate;
+    [SerializeField] private float _coolDownRate;
+    [SerializeField] private string _emotionName;
+    private float crashOutCooldownRate;
+
+    private float _defenseModifier;
+    private float _speedModifier;
+    private float _attackModifier;
+
+    private bool _canAwake;
 
     #endregion
 
     #region Unity Methods 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        
+    }
+
     void Start()
     {
         
@@ -30,6 +52,12 @@ public class Emotion : MonoBehaviour
     #endregion
 
     #region Public Methods 
+    public enum EmotionState
+    {
+        Awake,
+        Sleep,
+        CrashOut
+    }
     #endregion
 
     #region Private Methods 
--- Assets/Prototype/1. Code/Behaviors/PlayerHealth.cs
+++ Assets/Prototype/1. Code/Behaviors/PlayerHealth.cs
@@ -2,17 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     #region Public Variables 
+    public float DefenseModifier { get => _defenseModifier; set => _defenseModifier = value; }
     #endregion
 
     #region Private Variables 
+    [SerializeField] private float _playerMaxHealth;
+    [SerializeField] private float _playerCurrentHealth;
+    [SerializeField] private TMP_Text _playerHealthText;
+    private float _defenseModifier = 1f;
+    private bool _isDead = false;
     #endregion
 
     #region Unity Methods 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnEnable()
+    {
+        GameManager.OnGameStateChanged += GameManagerOnGameStateChange;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= GameManagerOnGameStateChange;
+    }
     void Start()
     {
         
@@ -21,14 +36,52 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePlayerHealth();
     }
     #endregion
 
     #region Public Methods 
+    public void PlayerTakeDamage(float pDamage)
+    {
+        _playerCurrentHealth -= pDamage * _defenseModifier;
+    }
+
+    public void PlayerGainHealth(float pHealthAmount)
+    {
+        _playerCurrentHealth += pHealthAmount;
+    }
     #endregion
 
     #region Private Methods 
+    private void GameManagerOnGameStateChange(GameManager.GameState pState)
+    {
+        if (pState == GameManager.GameState.SetUp)
+        {
+            SetPlayerHealth();
+        }
+    }
+
+    private void SetPlayerHealth()
+    {
+        _playerCurrentHealth = _playerMaxHealth;
+
+        if (_playerCurrentHealth >= _playerMaxHealth)
+            _playerCurrentHealth = _playerMaxHealth;
+    }
+
+    private void UpdatePlayerHealth()
+    {
+        _playerHealthText.text = Mathf.Round(_playerCurrentHealth).ToString();
+
+        if(_playerCurrentHealth <= 0)
+        {
+            _playerCurrentHealth = 0f;
+            _isDead = true;
+        }
+
+        if (_isDead)
+            GameManager.Instance.UpdateGameState(GameManager.GameState.Dead);
+    }
     #endregion
 
     #region Coroutines
